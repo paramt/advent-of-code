ef8f15cb1183cb6030accbb49716b78292f991b3
Param Thakkar <pht0104@gmail.com>
2022-12-14 23:34:57 -0500

    Day 12

---
+++ day12/part2.py
@@ -0,0 +1,42 @@
+# Set end
+END = {"x": 119, "y": 20}
+
+grid = []
+line = input()
+
+while line != "0":
+    grid.append(list(line.replace("S", "a").replace("E", "z")))
+    line = input()
+
+def possible_steps(pos):
+    elevation = ord(grid[pos["y"]][pos["x"]])
+
+    steps = []
+
+    if pos["y"] < len(grid)-1 and ord(grid[pos["y"]+1][pos["x"]]) >= elevation - 1:
+        steps.append({"x": pos["x"], "y": pos["y"]+1})
+    if pos["y"] >= 1 and ord(grid[pos["y"]-1][pos["x"]]) >= elevation - 1:
+        steps.append({"x": pos["x"], "y": pos["y"]-1})
+    if pos["x"] < len(grid[0])-1 and ord(grid[pos["y"]][pos["x"]+1]) >= elevation - 1:
+        steps.append({"x": pos["x"]+1, "y": pos["y"]})
+    if pos["x"] >= 1 and ord(grid[pos["y"]][pos["x"]-1]) >= elevation - 1:
+        steps.append({"x": pos["x"]-1, "y": pos["y"]})
+
+    return steps
+
+visited = [[END, 0]]
+queue = [[END, 0]]
+
+while queue: 
+    node, depth = queue.pop(0)
+
+    if grid[node["y"]][node["x"]] == "a": 
+        print("done!")
+        print(depth)
+        break
+
+    for n in possible_steps(node):
+        if n not in [visited_node[0] for visited_node in visited]: 
+            visited.append([n, depth+1])
+            queue.append([n, depth+1])
+
